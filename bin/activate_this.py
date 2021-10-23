32088ee2f29cf1c6ee41c42c896e00d953d5afea
Rachet Mudnur <rmudnur@gmail.com>
2021-10-23 15:22:42 +0530

    Updated activate_this.py

--- bin/activate_this.py
+++ bin/activate_this.py
@@ -14,13 +14,6 @@ try:
 except NameError:
     raise AssertionError("You must use exec(open(this_file).read(), {'__file__': this_file}))")
 
-bin_dir = os.path.dirname(abs_file)
-base = bin_dir[: -len("bin") - 1]  # strip away the bin part from the __file__, plus the path separator
-
-# prepend bin to PATH (this file is inside the bin directory)
-os.environ["PATH"] = os.pathsep.join([bin_dir] + os.environ.get("PATH", "").split(os.pathsep))
-os.environ["VIRTUAL_ENV"] = base  # virtual env is right above bin directory
-
 # add the virtual environments libraries to the host python import mechanism
 prev_length = len(sys.path)
 for lib in "../lib/python3.7/site-packages".split(os.pathsep):
